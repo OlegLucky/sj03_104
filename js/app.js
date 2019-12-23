cad4bd4792403d9b0c40c8f068bacb284eb7c2b6
OlegLucky <iamrichestman@gmail.com>
2019-12-23 18:45:24 +0200

    test 08

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 console.log('App was loaded');
+
+const person = confirm("Who are you?");
+const age = prompt("Your age");
+// if(person) {
+//     if(age > 20 ){
+//         if(age < 40) {
+//             console.log("ok");
+//         }
+//     }
+// }
+
+
+if(person && age > 20  && age < 40){
+    console.log("ok");
+}
